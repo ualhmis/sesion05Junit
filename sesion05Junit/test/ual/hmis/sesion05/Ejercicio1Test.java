71527d1bf83c30859a25da032f3c17491ac3f780
Administrator <Administrator@10.0.0.5>
2019-04-24 10:50:44 +0200

    primo sale primo

--- sesion05Junit/test/ual/hmis/sesion05/Ejercicio1Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/Ejercicio1Test.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class Ejercicio1Test {
 
 	@ParameterizedTest (name = "{index} => Con entrada ({0}) sale {1}")
-	@CsvSource({"17, 1",
+	@CsvSource({"17, 17",
 //				"5, 1",
 //				"6, 1",
 //				"14, 7"
